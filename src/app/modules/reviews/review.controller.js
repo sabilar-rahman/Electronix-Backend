620d7dc580fa9678106513799199fe553779e1d7
sabilar-rahman <sabilarrahman36@gmail.com>
2024-11-26 02:24:18 +0600

    Refactor : post a review

--- src/app/modules/reviews/review.controller.js
+++ src/app/modules/reviews/review.controller.js
@@ -1,3 +1,4 @@
+const Products = require("../products/product.model");
 const Reviews = require("./review.model");
 
 const postReview = async (req, res) => {
@@ -15,32 +16,40 @@ const postReview = async (req, res) => {
             existingReview.rating = rating;
             await existingReview.save();
 
-            return res.status(400).send({ message: "Review already exists" });
+
         } else {
-            const review = await Reviews.create({ comment, rating, userId, productId });
-            if (review.length > 0) {
-                const totalRating = review.reduce((acc, review) => ((acc += review.rating), 0));
-                const averageRating = totalRating / review.length;
+            const newReview = new Reviews({ comment, rating, userId, productId });
 
-                // await Products.updateOne({ _id: productId }, { $set: { rating: averageRating } });
-
-                const product = await Products.findById(productId);
-                if(product) {    
-                    product.rating = averageRating;
-                    await product.save({validateBeforeSave: false});
-                }else{
-                    return res.status(404).send({ message: "Product not found" });
-                }
-
-            }
-            res.status(200).send({ message: "Review created successfully", review });
+            await newReview.save();
         }
 
+        const review = await Reviews.find({ productId});
 
-    } catch (error) {
+
+        if (review.length > 0) {
+            const totalRating = review.reduce((acc, review) => ((acc + review.rating), 0));
+            const averageRating = totalRating / review.length;
+
+
+
+            const product = await Products.findById(productId);
+            if (product) {
+                product.rating = averageRating;
+                await product.save({ validateBeforeSave: false });
+            } else {
+                return res.status(404).send({ message: "Product not found" });
+            }
+
+        }
+        res.status(200).send({ message: "Review created successfully", review });
+    }
+
+
+    catch (error) {
         console.error("Error creating review:", error);
         res.status(500).send({ message: "Failed to create review", error });
     }
 };
+
 
 module.exports = { postReview };
